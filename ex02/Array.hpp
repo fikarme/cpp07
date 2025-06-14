8c71587f25f891217867ec2be90cbfba4f375ec7
fikarme <fikarme@pm.me>
2025-06-14 16:11:47 +0300

    gonn kill myself

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,71 +1,77 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-# include <iostream>
+#include <cstddef> // For NULL
+#include <stdexcept> // For std::out_of_range
+#include <iostream> // for std::cerr
+#include <cstdlib>
 
-// This is a template class, so it can be used with any type.
-template<typename T>
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::out_of_range;
+using std::exception;
+using std::srand;
+using std::rand;
 
-class	Array {
-	public:
-		Array< T >( void ) 									// Default constructor
-		{
-			this->_size = 0;
-			this->_arr = NULL;
-			//std::cout << "Default Constructor invoked" << std::endl;
-			return ;
-		}
+template <typename T>
+class Array {
+public:
+    Array<T>(void) : _size(0), _arr(NULL) {}
 
-		Array< T >( unsigned int n ) 					// Constructor. It is unsigned int because it is the size of the array and size can't be negative value.
-		{
-			this->_size = n;
-			this->_arr = new T[n];	// We must use new operator to create an array as pdf dictates. Because preventive allocation is forbidden. Preventive allocation is allocating memory for an array before we know how many elements we need.
-			//std::cout << "Constructor invoked" << std::endl;
-			return ;
-		}
+    Array<T>(unsigned int N) : _size(N), _arr(NULL) {
+        if (N > 0)
+            _arr = new T[N];
+    }
 
-		Array< T >( const Array & other )			// Copy constructor
-		{
-			*this = other;
-			return ;
-		}
+    Array<T>(const Array<T>& Other) : _size(Other._size), _arr(NULL) {
+        if (_size > 0) {
+            _arr = new T[_size];
+            for (unsigned int i = 0; i < _size; ++i)
+                _arr[i] = Other._arr[i];
+        }
+    }
 
-		~Array< T >( void )						// Destructor
-		{
-			if (this->_size > 0)
-				delete[] this->_arr;
-		};
+    ~Array<T>(void) {
+        delete[] _arr;
+    }
 
-		Array & operator=(const Array & other)			// Assignation operator
-		{
-			if (this == &other)			// If the object is assigned to itself, return the object.
-				return (*this);
-			this->_size = other.getSize();
+    Array<T>& operator=(const Array<T>& Other) {
+        if (this == &Other)
+            return *this;
 
-			// If the array size is valid, create a new array.
-			if (this->_size > 0)
-				this->_arr = new T[this->_size];
+        delete[] _arr;
 
-			// Copy the array. One by one. Each element we copy it to the new array.
-			for (unsigned int i = 0; i < this->_size; i++)
-				this->_arr[i] = other._arr[i];
-			return (*this);
-		}
+        _size = Other._size;
+        _arr = NULL;
 
-		T & operator[](unsigned int index)		// Operator overloading. Its' used to access the elements of the array.
-		{
-			if (index >= getSize())
-				throw std::out_of_range("Out of the range!");
-			return (this->_arr[index]);			// Return the element at index.
-		}
+        if (_size > 0) {
+            _arr = new T[_size];
+            for (unsigned int i = 0; i < _size; ++i)
+                _arr[i] = Other._arr[i];
+        }
+        return *this;
+    }
 
-		unsigned int	getSize(void) const {
-			return (this->_size);
-		}
+    T& operator[](unsigned int Index) {
+        if (Index >= _size)
+            throw out_of_range("Index out of bounds");
+        return _arr[Index];
+    }
 
-	private:
-		unsigned int	_size;
-		T*				_arr;
+    const T& operator[](unsigned int Index) const {
+        if (Index >= _size)
+            throw out_of_range("Index out of bounds");
+        return _arr[Index];
+    }
+
+    unsigned int GetSize(void) const {
+        return _size;
+    }
+
+private:
+    unsigned int _size;
+    T* _arr;
 };
 
 #endif
